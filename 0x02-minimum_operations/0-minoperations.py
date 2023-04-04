0c864161fad19e0c6f0ff8743dcf7adecf5a07fd
Awet11-Tesfay <hailegb2021@gmail.com>
2023-04-04 23:26:21 +0300

    min operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+''' Description: In a ext file there is a single cahracter H your text
+                 editor can execue only two operations in this file copy all
+                 and paste given a number n write a method that calculates the
+                 fewest number of operations needed to result in exactly n H
+                 characters in the file
+'''
+
+
+def minOperations(n):
+    if n < 2 or type(n) is not int:
+        return 0
+    
+    a = 0
+    x = 2
+    while (x <= n):
+        if not(n % x):
+            n = int(n / x)
+            a += x
+            x = 1
+    return a
