47a24184594578563c0e4cb0fc10b241d36505d1
Awet11-Tesfay <hailegb2021@gmail.com>
2023-04-20 22:47:51 +0300

    utf-8

--- 0x04-utf_validation/0-validate_utf8.py
+++ 0x04-utf_validation/0-validate_utf8.py
@@ -1,40 +1,33 @@
 #!/usr/bin/python3
-"""Determines a valid UTF-8 encoding"""
+""" Determines a valid UTF-8 encoding """
 
 
 def validUTF8(data):
-    """
-    bit1 checks if significant byte is 1
-    bit2 checks if second significant byte is 0
-    nbytes keeps track of how many 1s before 0 occurs
-    data represented by a list of integers to check
-    """
+    """ Number of bytes in the current UTF-8 character """
+    num_bytes = 0
 
-    bit1 = 1 << 7
-    bit2 = 1 << 6
-    nbytes = 0
+    for i in data:
 
-    if not data or len(data) == 0:
-        return True
+        bin_rep = format(i, '#010b')[-8:]
 
-    for num in data:
-        bit = 1 << 7
-        if nbytes == 0:
-            while (bit & num):
-                nbytes += 1
-                bit = bit >> 1
+        if num_bytes == 0:
 
-            if nbytes == 0:
+            for bit in bin_rep:
+                if bit == '0':
+                    break
+                num_bytes += 1
+
+            if num_bytes == 0:
                 continue
-            if nbytes == 1 or nbytes > 4:
+
+            if num_bytes == 1 or num_bytes > 4:
                 return False
+
         else:
 
-            if not (num & bit1 and not (num & bit2)):
+            if not (bin_rep[0] == '1' and bin_rep[1] == '0'):
                 return False
-        nbytes -= 1
 
-    if nbytes:
-        return False
-    else:
-        return True
+        num_bytes -= 1
+
+    return num_bytes == 0
