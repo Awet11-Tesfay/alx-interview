e4c4169cfb6279c72ee4ea10ff80fb798691def0
Awet11-Tesfay <hailegb2021@gmail.com>
2023-04-20 22:05:32 +0300

    utf-8

--- 0x04-utf_validation/0-validate_utf8.py
+++ 0x04-utf_validation/0-validate_utf8.py
@@ -6,7 +6,7 @@
 def validUTF8(data):
     """ Return true if data is a valid UTF-8 encoding. else return false
     """
-    
+
     do1 = 1 << 7
     do2 = 1 << 6
     number_bytes = 0
@@ -14,11 +14,12 @@ def validUTF8(data):
     if not data or len(data) == 0:
         return True
 
-    for x in data:
-        m_number_byte = 1 << 7
+    for digit in data:
+        nbit = 1 << 7
         if number_bytes == 0:
-            while (m_number_byte & x):
-                m_number_byte = m_number_byte >> 1
+            while (nbit & digit):
+                number_bytes += 1
+                nbit = nbit >> 1
 
             if number_bytes == 0:
                 continue
@@ -26,9 +27,9 @@ def validUTF8(data):
                 return False
         else:
 
-            if not (x & do1 and not (x & do2)):
+            if not (digit & do1 and not (digit & do2)):
                 return False
-            number_bytes -= 1
+        number_bytes -= 1
 
     if number_bytes:
         return False
