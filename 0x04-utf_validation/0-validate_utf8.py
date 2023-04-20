48073227b0deb1416a1c9e02df6eea1d48968d2a
Awet11-Tesfay <hailegb2021@gmail.com>
2023-04-20 21:40:56 +0300

    utf-8

---
+++ 0x04-utf_validation/0-validate_utf8.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+""" Write a method that determines if a given dataset represents a valid UTF-8
+"""
+
+
+def validUTF(data):
+    """ Return true if data is a valid UTF-8 encoding. else return false
+    """
+    do1 = 1 << 7
+    do2 = 1 << 6
+    number_bytes = 0
+
+    if not data or len(data) == 0:
+        return True
+
+    for x in data:
+        m_number_byte = 1 << 7
+        if number_bytes == 0:
+            while (m_number_byte & x):
+                m_number_byte = m_number_byte >> 1
+
+            if number_bytes == 0:
+                continue
+            if number_bytes == 1 or number_bytes > 4:
+                return False
+        else:
+
+            if not (x & do1 and not (x & do2)):
+                return False
+            number_bytes -= 1
+
+    if number_bytes:
+        return False
+    else:
+        return True
