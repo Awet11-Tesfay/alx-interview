bd17f329315184d7bede713c15dd4732e6cbbfc4
Awet11-Tesfay <hailegb2021@gmail.com>
2023-06-01 12:12:27 +0300

    'prime_game'

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,11 +4,11 @@
 
 
 def prime_game(n):
-    """ List of Prime numbers
+    """ List of the Prime numbers
     """
     game = []
-    gabe_b = [True] * (n + 1)
-    for m = in range(2, n + 1):
+    game_b = [True] * (n + 1)
+    for m  in range(2, n + 1):
         if (game_b[m]):
             game.append(m)
             for z in range(m, n + 1, m):
@@ -19,10 +19,10 @@ def prime_game(n):
 def isWinner(x, nums):
     """ Determine who the winner of each game is
     """
-    if y is None or nums is None or y == 0 or nums == []:
+    if x is None or nums is None or x == 0 or nums == []:
         return None
     Maria = Ben = 0
-    for a in range(y):
+    for a in range(x):
         prime = prime_game(nums[a])
         if len(prime) % 2 == 0:
             Ben += 1
